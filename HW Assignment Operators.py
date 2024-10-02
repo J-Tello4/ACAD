767b3346fcbdf779135f1501f9136a2fd6ec7e53
Jeff <jjtello1422@gmail.com>
2024-10-01 22:41:58 -0400

    Homework Assignment

---
+++ HW Assignment Operators.py
@@ -0,0 +1,16 @@
+## Homework
+# Write a Python script that:
+# - Calculates and prints the result of `(50 - 5*6) / 4`.
+# - Checks if `100` is not equal to `90` and prints the result.
+# - Uses `and`, `or`, and `not` to evaluate `((5 > 3) and (10 < 12)) or not(15 < 20)` and prints the outcome.
+
+Calculation = int(50 - (5*6))/ 4
+
+print(Calculation)
+
+check = 100 is not 90
+print(check)
+
+first = (5 >3) and (10 <12) or not (15 <30) 
+print(first)
+
